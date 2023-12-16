8f5298bf00a86185f3fa56e44daffca74770b8e4
siwat jermwatthana <siwat.j@hiveground.com>
2023-12-17 02:24:49 +0700

    [Feat]: Add proxy configuration for API requests

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,15 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3030",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
